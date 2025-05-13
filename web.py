9c32f238ffde702e306dc7a5968427eae9de20ae
khauta seabata <75206578+Kali01009@users.noreply.github.com>
2025-05-12 23:44:05 -0800

    web.py

--- web.py
+++ web.py
@@ -68,6 +68,6 @@ def send_hello():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
